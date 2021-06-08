6820dbbc23c76858c954de7493153543972c7231
kyruel <kyruel@yahoo.com>
2021-06-08 08:53:07 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+# Terraform State Storage to Azure Storage Container
+  backend "azurerm" {
+    resource_group_name   = "testing"
+    storage_account_name  = "terraformstorage75ff"
+    container_name        = "tfstatefiles"
+    key                   = "terraform-custom-vnet.tfstate"
+  }  
+}
+
+# 2. Terraform Provider Block for AzureRM
+provider "azurerm" {
+  features {
+
+  }
+}
+
+# 3. Terraform Resource Block: Define a Random Pet Resource
+resource "random_pet" "aksrandom" {
+
+}
+
---
+++ outputs.tf
@@ -0,0 +1,20 @@
+# Resource Group Outputs
+output "location" {
+  value = azurerm_resource_group.aks_rg.location
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.aks_rg.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks_rg.name
+}
+# Azure AKS Outputs
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks_cluster.name
+}
